9d2f9da35e98f01daf918fad4b63eec5b9ce1775
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-05-27 19:53:29 -0300

    dev

--- src/main/kotlin/net/hyren/factions/listeners/connection/PlayerJoinListener.kt
+++ src/main/kotlin/net/hyren/factions/listeners/connection/PlayerJoinListener.kt
@@ -1,6 +1,9 @@
 package net.hyren.factions.listeners.connection
 
+import net.hyren.core.shared.CoreProvider
+import net.hyren.core.shared.groups.Group
 import net.hyren.factions.FactionsProvider
+import net.hyren.factions.alpha.misc.player.list.data.PlayerList
 import net.hyren.factions.misc.player.list.updatePlayerList
 import org.bukkit.Bukkit
 import org.bukkit.event.*
@@ -15,15 +18,23 @@ class PlayerJoinListener : Listener {
     fun on(
         event: PlayerJoinEvent
     ) {
-//        val player = event.player
-//
-//        PlayerList.hideCommonPlayers(player)
-//
+        val player = event.player
+        val user = CoreProvider.Cache.Local.USERS.provide().fetchById(player.uniqueId)!!
+        val factionUser = FactionsProvider.Cache.Local.FACTION_USER.provide().fetchByUserId(user.id)
+
         Bukkit.getOnlinePlayers().forEach {
             FactionsProvider.Cache.Local.FACTION_USER.provide().fetchByUserId(it.uniqueId)?.let {
-                it.updatePlayerList()
+                when {
+                    user.hasGroup(Group.HELPER) -> it.updatePlayerList()
+                    it.hasFaction() && factionUser?.hasFaction() == true && it.factionId == factionUser.factionId -> it.updatedAt
+                    else -> Unit
+                }
             }
+
+            PlayerList.hideCommonPlayers(it)
         }
+
+        PlayerList.hideCommonPlayers(player)
     }
 
 }

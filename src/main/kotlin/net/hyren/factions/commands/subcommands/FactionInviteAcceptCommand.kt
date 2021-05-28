2bacde1d1a442be266e932aefe1412077528d379
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-05-27 22:51:47 -0300

    dev

--- src/main/kotlin/net/hyren/factions/commands/subcommands/FactionInviteAcceptCommand.kt
+++ src/main/kotlin/net/hyren/factions/commands/subcommands/FactionInviteAcceptCommand.kt
@@ -13,6 +13,7 @@ import net.hyren.factions.YOU_ALREADY_HAVE_FACTION
 import net.hyren.factions.echo.packet.*
 import net.hyren.factions.misc.player.list.updatePlayerList
 import net.hyren.factions.user.storage.dto.UpdateFactionUserDTO
+import net.md_5.bungee.api.chat.BaseComponent
 import net.md_5.bungee.api.chat.TextComponent
 import net.md_5.bungee.api.chat.ComponentBuilder
 import org.bukkit.command.CommandSender
